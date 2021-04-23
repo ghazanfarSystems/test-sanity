5b0ba5daf37cbcf405fd187fa3e5344b59664a40
Muhammad Ghazanfar Ali <77492127+ghazanfarSystems@users.noreply.github.com>
2021-04-23 15:19:15 +0500

    Update index.js

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -99,7 +99,7 @@ const IndexPage = (props) => {
         <h1 hidden>Welcome to {site.title}</h1>
         {postNodes && (
           <BlogPostPreviewList
-            title="Latest blog posts"
+            title="Latest blog posts testing now"
             nodes={postNodes}
             browseMoreHref="/archive/"
           />
